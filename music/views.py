8cca1df041f770ff793079af741015bc90a28a25
hailMeh <Sultanhabibi@mail.ru>
2022-05-18 16:24:31 +0300

    view change

--- music/views.py
+++ music/views.py
@@ -6,12 +6,19 @@ from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMix
 from django.shortcuts import render, get_object_or_404, redirect
 from django.db.models import Q
 
+
 class MusicListView(LoginRequiredMixin, ListView):
     model = Music
     template_name = 'music/music_list.html'
     paginate_by = 2
     context_object_name = 'music_list'
     raise_exception = True  # Если пользователь неавторизован, то доступ запрещен
+    queryset = Music.objects.all().order_by('-time_create') # Отображение на главной в обратном порядке. Select_related для оптимизации загрузки из БД
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'All albums'
+        return context
 
 
 class MusicDetailView(LoginRequiredMixin, PermissionRequiredMixin, DetailView):
@@ -22,6 +29,11 @@ class MusicDetailView(LoginRequiredMixin, PermissionRequiredMixin, DetailView):
     raise_exception = True  # Если пользователь неавторизован, то доступ запрещен
     permission_required = 'music.special_status'  # созданный ранее доступ через Meta в моделях
 
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = context['music']
+        return context
+
 
 class AddMusicView(LoginRequiredMixin, CreateView):
     form_class = AddMusicForm
@@ -29,6 +41,11 @@ class AddMusicView(LoginRequiredMixin, CreateView):
     success_url = reverse_lazy(
         'music_list')
     raise_exception = True  # Если пользователь неавторизован, то доступ запрещен
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Add new album'
+        return context
 
 
 def authneed(request, exception):  # Страница не найдена
@@ -55,9 +72,25 @@ class CategoryView(ListView):
         context = super().get_context_data(**kwargs)
         context['title'] = 'Category - ' + str(context['music'][0].category)
         context['category'] = Music.objects.filter(category__slug=self.kwargs['slug']).select_related('category')
+        context['numbers_of_albums'] = Music.objects.filter(category__slug=self.kwargs['slug']).select_related('category').count()
         return context
 
     def get_queryset(self):  # ОРМ можно применять через служебную функцию
         return Music.objects.filter(category__slug=self.kwargs['slug'], is_published=True).select_related('category')
 
 
+''' ФУНКЦИОНАЛЬНАЯ ФОРМА ДЛЯ ДОБАВЛЕНИЯ КНИГИ
+def addbook(request):
+    if request.method == 'POST':
+        form = AddBookForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+    else:
+        form = AddBookForm()
+    context = {
+        'title': 'add_book',
+        'form': form
+    }
+    return render(request, 'women/add_book.html', context=context)
+'''
